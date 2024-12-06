768836930c8bf64e0f1fbdc0435eeb996046721e
Muhammad Hamza <141921986+Hamzakhanzada@users.noreply.github.com>
2024-12-06 15:57:00 +0000

    add data types

---
+++ 0_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+const firstName = "Hamza";
+const myAge = 19;
+const dataSubmit = true;
+const largeNumber = 9007199254740995n;
+let state;
+const define = null;
+
+console.log(typeof firstName,typeof myAge,typeof dataSubmit,typeof largeNumber,typeof state,typeof define);
+
+
