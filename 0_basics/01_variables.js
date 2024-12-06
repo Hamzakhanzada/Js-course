f86ddabbcb323255cf4b64e326b73ec51538fb1e
Muhammad Hamza <141921986+Hamzakhanzada@users.noreply.github.com>
2024-12-06 13:59:36 +0000

    add variables

---
+++ 0_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 34850;
+let accountEmail = "khanzadahamza76@gmail.com";
+var accountPassword = "12345";
+accountCity = "Karachi";
+let accountState;
+// accountId = 3; not allowed
+accountEmail = "hamza@gmail.com";
+accountPassword = "4534";
+accountCity = "Hyderabad";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+prefer not to use var 
+because of issue in block and functional Scope
+
+*/
+
+
+
+
