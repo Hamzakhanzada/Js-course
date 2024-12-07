0c14214413ac92a11b8edb4f2fe9d77ac276415b
Muhammad Hamza <141921986+Hamzakhanzada@users.noreply.github.com>
2024-12-07 08:08:21 +0000

    conversion of datatypes

---
+++ 0_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let scope = "hamza";
+
+// console.log(typeof scope);
+// console.log(scope);
+
+let numberToString = String(scope);
+
+// console.log(numberToString);
+// console.log(typeof numberToString);
+
+
+let stringToNumber = Number(scope);
+console.log(stringToNumber);
+console.log(typeof stringToNumber);
+
+let someNumber = 1;
+
+let numberToBoolean = Boolean(someNumber);
+console.log(numberToBoolean);
+console.log(typeof numberToBoolean);
+
+
+
