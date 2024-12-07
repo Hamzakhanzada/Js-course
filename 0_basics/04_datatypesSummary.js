ad4850946a8b38793f67478aafb9ae75d186a728
Muhammad Hamza <141921986+Hamzakhanzada@users.noreply.github.com>
2024-12-07 13:32:55 +0000

    add summary datatypes

---
+++ 0_basics/04_datatypesSummary.js
@@ -0,0 +1,16 @@
+
+let string = "Hello World";
+console.log(string);
+let number = 90;
+console.log(number);
+let boolean = true;
+console.log(boolean);
+let bigint = 5938450938504n;
+console.log(typeof bigint);
+let userId = Symbol('123');
+console.log(userId);
+let userId2 = Symbol('123');
+console.log(userId2);
+console.log(userId === userId2);
+
+
