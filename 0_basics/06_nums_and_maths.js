9a747cb9d0d07b3f7fa4a6fe2c5b085b4d4336a6
Muhammad Hamza <141921986+Hamzakhanzada@users.noreply.github.com>
2024-12-09 08:20:11 +0000

    add maths and number

---
+++ 0_basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+const score = 700;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+////////++++++++++++++++++Maths+++++++++++++++++++++++++++//////////
+// console.log(Math);
+// console.log(Math.abs(-4)); //convert to negative value to positive value;
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()* 10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()* (min-max) +1) + max);
+
+
+
+
+
+
+
