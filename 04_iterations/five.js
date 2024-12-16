abf08fda8236bf831ace7356c77eac4e408d10aa
Muhammad Hamza <141921986+Hamzakhanzada@users.noreply.github.com>
2024-12-16 08:16:49 +0000

    add foreach loop

---
+++ 04_iterations/five.js
@@ -0,0 +1,33 @@
+const coding = ["html","css","js"];
+
+coding.forEach( (item) => {
+    console.log(item);
+});
+
+
+coding.forEach( (item,index,array) => {
+    console.log(item,index,array);
+
+});
+
+
+const myCoding = [
+    {
+        language : "javascript",
+        languageFile: "js"
+    },
+    {
+        language : "java",
+        languageFile: "java"
+
+    },
+    {
+        language : "phython",
+        languageFile: "py"
+
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.language);
+})
