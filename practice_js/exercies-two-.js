66826456805a14a48c23b122e8d5bf787e48aa00
Hamza <khanzadahamza76@gmail.com>
2024-12-22 16:09:06 +0500

    conditonals practice

---
+++ practice_js/exercies-two-.js
@@ -0,0 +1,21 @@
+// Task for You
+// Write a program to:
+
+// Declare two numbers x and y.
+// Perform all arithmetic operations on them.
+// Compare if x is greater than y.
+// Use logical operators to check conditions like x > 5 && y < 10.
+// Share your code and output. Main tumhara review karunga aur agle concepts pe le kar jaunga! 🚀
+
+let x = 10;
+let y = 5;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x > y);
+console.log(x < y);
+console.log(x == y);
+console.log(x === y);
+console.log(x > y && x < y);
+console.log(x > y || x < y);
