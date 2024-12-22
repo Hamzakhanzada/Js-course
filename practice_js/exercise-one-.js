dea56259fc10f97282f9a03358e50a8eb4ed8bf9
Hamza <khanzadahamza76@gmail.com>
2024-12-22 13:28:27 +0500

    js practice set one

---
+++ practice_js/exercise-one-.js
@@ -0,0 +1,19 @@
+// 4. Practice Exercise
+// Write JavaScript code for the following:
+
+// Declare a variable myName and assign your name to it.
+// Declare two numbers, a and b, and store their sum in a variable called sum.
+// Create a variable isSunny and set it to true.
+
+let myName = 'Hamza';
+
+let a = 2;
+
+let b = 3;
+
+let sum = a+b;
+
+let isSunny = true;
+console.log('My Name:', myName);
+console.log('Sum of a and b:', sum);
+console.log('Is it sunny?', isSunny);
