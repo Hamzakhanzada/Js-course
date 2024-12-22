b91ad9d734ce048c8fed8400b81a74757a3ed5fc
Hamza <khanzadahamza76@gmail.com>
2024-12-22 16:59:37 +0500

    practice about functions

---
+++ practice_js/excercise-five.js
@@ -0,0 +1,11 @@
+function greeting (name) {
+    console.log`Hello ${name} `
+}
+
+greeting('Hamza')
+
+function addTwoNums (num1,num2) {
+    return num1 + num2
+}
+let sum = addTwoNums(8,2)
+console.log(sum)
