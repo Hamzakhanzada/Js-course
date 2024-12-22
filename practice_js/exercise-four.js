4082fafe3f38b452cbd1e501cf3cb869612f16ac
Hamza <khanzadahamza76@gmail.com>
2024-12-22 16:09:36 +0500

    loops practice

---
+++ practice_js/exercise-four.js
@@ -0,0 +1,18 @@
+// loops practice set
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+    
+}
+
+let i = 1
+while (i <= 10 ) {
+    console.log(i)
+    i ++
+}
+
+let j = 1
+do {
+    console.log('Javascript fun',j)
+    j++
+} while (j < 4);
