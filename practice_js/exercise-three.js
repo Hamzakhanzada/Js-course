153bd5f307ad2440835f2310f3bbe6aed93b79f6
Hamza <khanzadahamza76@gmail.com>
2024-12-22 16:08:11 +0500

    if else practice set

---
+++ practice_js/exercise-three.js
@@ -0,0 +1,20 @@
+// Check if a number is positive, negative, or zero.
+// Declare a variable number.
+// Use if-else or else if to check whether the number is positive, negative, or zero.
+// Print out a message for each condition:
+// If the number is positive, print "The number is positive".
+// If the number is negative, print "The number is negative".
+// If the number is zero, print "The number is zero".
+
+let number = -5 
+
+if (number > 0) {
+    console.log("The number is positive")
+
+} else if (number === 0) {
+    console.log("The number is zero")
+} else {
+    console.log("The number is negative")
+
+}
+
